2a94d2e529274e498c81c3c90b3cc0e1233a7a2e
damon@firefly <damon@firefly>
2013-09-10 12:40:36 +1000

    added node server

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,7 @@
 /*globals requirejs: false, window: false*/
 
 requirejs.config({
-  baseUrl: 'scripts',
+  baseUrl: 'js',
   'paths': {
     'jquery': 'http://code.jquery.com/jquery-2.0.3.min' 
   }
@@ -19,10 +19,12 @@ requirejs(['jquery', 'balls'], function($, Balls) {
   var balls = new Balls({ left: 0, top: 0, right: canvas.width, bottom: canvas.height }, { num: 20 });
 
   window.setInterval(function() {
+
     ctx.canvas.width  = window.innerWidth;
     ctx.canvas.height = window.innerHeight;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     balls.move({ left: 0, top: 0, right: canvas.width, bottom: canvas.height });
     balls.draw(ctx);
+
   },1000/60);
 });
--- test/lib/require_config.js
+++ test/lib/require_config.js
@@ -1,5 +1,5 @@
 require.config({
-  baseUrl: '/test/scripts', // jstestdriver serves the files under /test/
+  baseUrl: '/test/public/js', // jstestdriver serves the files under /test/
   'paths': {
     'jquery': 'http://code.jquery.com/jquery-2.0.3.min' 
   }
